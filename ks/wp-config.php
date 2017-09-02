622ba6b82e7d5b233e9710590ff420e7271406d5
gaodonglai <31536061+gaodonglai@users.noreply.github.com>
2017-09-02 18:13:02 +0800

    Revert "更改数据库链接"
    This reverts commit 18c963a99db9648a3326d46a9f6e5080c4af6e96.

--- ks/wp-config.php
+++ ks/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'cp');
+define('DB_NAME', 'ks');
 
 /** MySQL数据库用户名 */
 define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', '452376leng');
+define('DB_PASSWORD', 'root');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
